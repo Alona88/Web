d7d4423e18d6529cf072d97bafb7360a22417fb6
Alona88 <126814810+Alona88@users.noreply.github.com>
2023-12-24 20:49:49 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://allo.ua/");
+        WebElement input = driver.findElement(By.xpath("//input[@id='search-form__input']"));
+        input.sendKeys("Hello");
+        driver.quit();
+    }
+    }
